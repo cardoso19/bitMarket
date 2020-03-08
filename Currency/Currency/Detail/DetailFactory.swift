b94e6153199b6af06b22ece2ee9fb4b512e2e2de
Matheus Kuhn <matheuscardosokuhn@hotmail.com>
2020-03-08 01:18:05 -0300

    Finish the detail view

---
+++ Currency/Currency/Detail/DetailFactory.swift
@@ -0,0 +1,9 @@
+//
+//  DetailFactory.swift
+//  Currency
+//
+//  Created by Matheus Cardoso Kuhn on 07/03/20.
+//  Copyright © 2020 Matheus Cardoso Kuhn. All rights reserved.
+//
+
+import Foundation
